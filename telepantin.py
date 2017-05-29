111ccdfa9904256bb4451fe96607f0c1f44c3407
msarch <msarch@free.fr>
2017-05-30 00:05:18 +0200

    using Points (x,y tuple) for coordinates to  improve readability (wip)

--- telepantin.py
+++ telepantin.py
@@ -2,12 +2,16 @@
 # -*- coding: iso-8859-1 -*-
 # http://www.github.com/msarch/slide
 
-# {{{ -------------------- STANDARD ENGINE SECTION (rev 2) --------------------
+DEBUG = True
+
+# {{{ -------------------- STANDARD ENGINE SECTION (rev 5) --------------------
 import math
 import pyglet
 from pyglet.gl import *
+from collections import namedtuple
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 1280,800
+Point = namedtuple('Point', 'x y')
+ORIGIN = Point(0,0)
 TWOPI = 2*math.pi
 OMEGA = 0.5 * 360.00                # angular velocity 0.5= 0.5rev/s
 alpha = 0.0                         # flywheel initial angle
@@ -15,11 +19,11 @@ vis = 1                             # visibility switch
 black = (  0,   0,   0, 255)
 white = (255, 255, 255, 255)
 
-
 # Pyglet Window stuff ---------------------------------------------------------
 batch = pyglet.graphics.Batch()  # holds all graphics
 config = Config(sample_buffers=1, samples=4,depth_size=16, double_buffer=True, mouse_visible=False)
-window = pyglet.window.Window(fullscreen=True, config=config)
+window = pyglet.window.Window(fullscreen=not(DEBUG), config=config)
+SCREEN_WIDTH, SCREEN_HEIGHT = window.width, window.height
 
 glClearColor(*black)  # background color
 glEnable(GL_LINE_SMOOTH)
@@ -46,77 +50,80 @@ class Sketch(pyglet.graphics.Group): # subclass with position/rotation ability
     '''
     'sketches' are regular pyglet graphics.Groups whom 'set_state' and
     'unset_state' methods are used to add move and rotate functionnalities.
-    Adding a shape to a group (batch.add) returns the matching vertex list,
-    color and vertex position are accessible through .colors and .vertices
     '''
-    def __init__(self,x,y,a):
+    def __init__(self,pos=ORIGIN, ro=0):  # pos.x,pos.y=coords, pos.a=rotation angle
         super(Sketch, self).__init__()
-        self.x, self.y, self.a = x, y, a  # x,y coords, rotation angle
+        self.pos,self.ro = pos, ro
 
     def set_state(self):
         glPushMatrix()
-        glTranslatef(self.x, self.y, 0)
-        glRotatef(self.a, 0, 0, 1) # GL rot. in degrees; x,y,z of rot. axis
+        glTranslatef(self.pos.x, self.pos.y, 0)
+        glRotatef(self.ro, 0, 0, 1) # GL rot. in degrees; x,y,z of rot. axis
 
     def unset_state(self):
         glPopMatrix()
 
 
-# Vertex transform functions --------------------------------------------------
-def transform(vtx, dx=0, dy=0, a=0):  #'hard' vertex transformation (translation/rotation)
-    cosa, sina = math.cos(TWOPI*a/360), math.sin(TWOPI*a/360)  #a given in deg
-    it = iter(vtx)
-    newverts = []
-    for i in xrange(len(vtx)/2):
-        x = it.next()
-        y = it.next()
-        newverts.append(cosa*x - sina*y + dx)
-        newverts.append(sina*x + cosa*y + dy)
-    return (newverts)
+# Vertex afine transform function ---------------------------------------------
+'''
+take a list of x,y coordinates as Points OR tuples,
+applies 2D rotation+translation to each couple of coords
+returns a list of Points
+'''
+def transform(vtx, dx=0, dy=0, ro=0):  #'hard' vertex transformation (translation/rotation)
+    cosro, sinro = math.cos(TWOPI*ro/360), math.sin(TWOPI*ro/360)  #a in degrees
+    return([Point(cosro*v[0]-sinro*v[1]+dx,sinro*v[0]+cosro*v[1]+dy) for v in vtx])
 
+flatten = lambda l: [item for sublist in l for item in sublist]
 
 # }}} --------------------- END OF STANDARD ENGINE SECTION --------------------
 
-# {{{ -------------------- STANDARD SHAPES SECTION (rev 1) --------------------
+
+
+# {{{ -------------------- STANDARD SHAPES SECTION (rev 3) --------------------
+# Adding a shape to a sketch(i.e. group) with batch.add returns a vertex list,
+# Color and vertex positions are later accessible with .colors and .vertices
+# Removing a shapes is done with vertex_list.delete(...)
 # Circle, outline only --------------------------------------------------------
-def circle(radius, color, sketch, dx=0, dy=0, a=0):
-        # number of divisions per ∏ rads (half the circle)
-    stepangle = TWOPI / (int(radius / 5) + 8)
-    vtx = [0, radius]  # create list and first element
+def circle(radius, color, sketch,dx=0, dy=0, ro=0):
+        # number of divisions per PI rads (half the circle)
+    stepangle = TWOPI / (int(radius / 5) + 8)  # number of segments depends of size
+    vtx = [Point(0, radius)]  # create list and first element
     phi = 0
     while phi < TWOPI:
         # because GL_LINES is used, we have to repeat same pt twice :
         # end of previous segment + start of next segment
-        vtx.extend([radius * math.sin(phi),radius * math.cos(phi),
-                 radius * math.sin(phi),radius * math.cos(phi)])
+        vtx.append(Point(radius * math.sin(phi),radius * math.cos(phi)))
+        vtx.append(Point(radius * math.sin(phi),radius * math.cos(phi)))
         phi += stepangle
-    vtx.extend([0, radius])  # add right side vertex
-    c=batch.add(len(vtx)/2, GL_LINES, sketch, 'v2f/static', 'c4B/static')
-    #circle=batch.add(numvtx, GL_LINE_STRIP, sk, 'v2f/static', 'c4B/static')
-    c.colors = color*(len(vtx)/2)
-    c.vertices = transform(vtx, dx=dx, dy=dy, a=a)
+    vtx.append(Point(0, radius))  # add right side vertex
+    c=batch.add(len(vtx), GL_LINES, sketch, 'v2f/static', 'c4B/static')
+    c.colors = color*(len(vtx))
+    c.vertices = flatten(transform(vtx, dx, dy, ro))
     return(c) # c is a vertex_list since batch.add() returns a vertex_list
 
 # Line ------------------------------------------------------------------------
-def line(x1, y1, x2, y2, color, pos, sketch, dx=0, dy=0, a=0):
-    l = batch.add(2, GL_LINES, still, 'v2f/static', 'c4B/static')
-    l.color = color*2
-    l.vertices=transform((x1, y1, x2, y2), dx=dx, dy=dy, a=a)
+def line(pt1, pt2, color, sketch, dx=0, dy=0, ro=0):
+    l = batch.add(2, GL_LINES, sketch, 'v2f/static', 'c4B/static')
+    l.colors= color*2
+    l.vertices=flatten(transform((pt1, pt2, dx,dy,ro)))
     return(l)
 
 # Rectangle, filed from triangles ---------------------------------------------
-def rec(w, h, color, sketch, dx=0, dy=0, a=0):
+def rec(w, h, color, sketch, dx=0, dy=0, ro=0):
     r = batch.add(6, pyglet.gl.GL_TRIANGLES, sketch, 'v2f/static', 'c4B/static')
     r.colors = color*6
-    r.vertices = transform((0,0,0,h,w,h,w,h,w,0,0,0), dx=dx, dy=dy, a=a)
+    r.vertices = flatten(transform(((0,0),(0,h),(w,h),(w,h),(w,0),(0,0)), dx, dy, ro))
     return(r) # batch.add() returns a vertex_list
 
 # }}} --------------------- END OF STANDARD SHAPES SECTION --------------------
 
-# {{{ ----------------------------- SCENE SECTION -----------------------------
 
-still = Sketch(SCREEN_WIDTH*0.5, SCREEN_HEIGHT*0.66, 0)  # 'default' still sketch
-wheel = Sketch(SCREEN_WIDTH*0.5, SCREEN_HEIGHT*0.66, 0)  # revolving
+
+# {{{ ----------------------------- SCENE SECTION -----------------------------
+wheel_center=Point(SCREEN_WIDTH*0.5, SCREEN_HEIGHT*0.66)
+wheel = Sketch(wheel_center)  # revolving sketch
+still = Sketch(wheel_center)  # 'default' still sketch
 
 # data and helper functions ---------------------------------------------------
 #      d--c
@@ -126,18 +133,21 @@ wheel = Sketch(SCREEN_WIDTH*0.5, SCREEN_HEIGHT*0.66, 0)  # revolving
 #  g---h  k---l ... -alf
 #      |  |
 #      i--j
+
 gu   = int(SCREEN_HEIGHT/110)  # overall drawing V size is 85*gu to fit screen
 kal, kaw, kae = 33 * gu, 11 * gu, 6 * gu  # use proportions of real kapla block
 rd1 = math.hypot(0.5 * kae, 0.5 * kae + kal)  # rad of outer verts (acdfgijl)
 rd2 = 0.5 * math.hypot(kae,kae)  # rad for inner verts (behk)
 alf  = math.atan(kae * 0.5 / kal)  # angle from center to a
-tk2 = kae/2
-span = tk2+kal
+tk2 = gu*kae/2
+span = gu*tk2+kal
+
 # kapla list of verts coords from a to l, by pairs: x1,y1,x2,y2, ...
-all_verts = [span, tk2,   tk2, tk2,    tk2, span,    -tk2, span,
-            -tk2, tk2,    -span,tk2,   -span, -tk2,  -tk2, -tk2,
-            -tk2,-span,   tk2, -span,  tk2, -tk2,    span,-tk2]
+all_verts = [(span, tk2),(tk2, tk2),(tk2, span),(-tk2, span),
+            (-tk2, tk2),(-span,tk2),(-span, -tk2),(-tk2, -tk2),
+            (-tk2,-span), (tk2, -span), (tk2, -tk2), (span,-tk2)]
 all_edges = tuple((i,i+1) for i in range (11))
+
 
 # kapla colors ----------------------------------------------------------------
 k_r  = (255,  69,   0, 255)  # red kapla
@@ -148,50 +158,55 @@ k_y  = (255, 214,   0, 255)  # yellow kapla
 
 def filter_by_edge_normal(edges):  # filters wrong direction facing edges
     #returns list of edges
-    pass
+    less=edges
+    return(less)
 
-def get_visible(edges):  # gets who is projecting, until hidden by next
+def visible_segments(edges):  # gets who is projecting, until hidden by next
     #returns 1 axis coordinates list
-    pass
-
+    cut=edges
+    return(cut)
 
 
 # shapes ----------------------------------------------------------------------
 # circles
-c = circle(radius=rd2, color=white, sketch=still)
-c = circle(radius=rd1, color=white, sketch=still)
+circle(radius=rd2, color=white, sketch=still)
+circle(radius=rd1, color=white, sketch=still)
 
 # four rects in a cross
-r1 = rec(w=kal, h=kae, color=k_r, sketch=wheel, dx=kae/2, dy=-kae/2)
-r2 = rec(w=kal, h=kae, color=k_b, sketch=wheel, dx=-kal-kae/2, dy= -kae/2)
-r3 = rec(w=kae, h=kal, color=k_g, sketch=wheel, dx=-kae/2, dy=kae/2)
-r4 = rec(w=kae, h=kal, color=k_y, sketch=wheel, dx=-kae/2, dy=-kal-kae/2)
+rec(w=kal, h=kae, color=k_r, sketch=wheel, dx=kae/2, dy=-kae/2)
+rec(w=kal, h=kae, color=k_b, sketch=wheel, dx=-kal-kae/2, dy=-kae/2)
+rec(w=kae, h=kal, color=k_g, sketch=wheel, dx=-kae/2, dy=kae/2)
+rec(w=kae, h=kal, color=k_y, sketch=wheel, dx=-kae/2, dy=-kal-kae/2)
 
-# generate a list of circles on ech vert, part of still sketch
-c=[i for i in xrange(len(all_verts)/2)]
-v = iter(all_verts)
-for i in xrange(len(all_verts)/2):
-    c[i]=circle(radius=10, color=white, sketch=still, dx=v.next(), dy=v.next())
+#generate empty list of rays on each vert
+#rays=[None for i in xrange(len(all_verts)/2)]  empty list of rays
+#for edge in all_edges:
+    #ray[i]=line(all_verts[edge[0]],
+                #all_verts[edge[1]],
+                #-SCREEN_WIDTH,
+                #all_verts[edge[1]],
+                #color=white, sketch=still)
+
 
 
 # updates ---------------------------------------------------------------------
 def updates(dt):
     # wheel is rotating
-    wheel.a = alpha
+    wheel.ro = alpha
 
     # only hard transform of each vert can give access to coordinates
-    transform(all_verts, a=alpha)
+    global all_verts
+    all_verts=transform(all_verts, ro=alpha)
 
-    # redraw new circle on each vert
-    v = iter(all_verts)
-    for i in xrange(len(all_verts)/2):
-        c[i].delete()
-        c[i]=circle(radius=15, color=white, sketch=still, dx=v.next(),dy=v.next())
-
-    filter_by_edge_normal(all_edges)
-    get_visible(all_edges)
-
+    source_segments= visible_segments(filter_by_edge_normal(all_edges))
     # lines droping from points
+    #live_lines=[]  #list of dynamically displayed elts recreated
+
+    #for edge in source_segments:
+        #live_lines.append(line(all_verts[edge[0]], all_verts[edge[1]],
+                               #-SCREEN_WIDTH, all_verts[edge[1]],
+                               #color=white, sketch=still))
+
 
 # }}} --------------------- END OF STANDARD SHAPES SECTION --------------------
 
